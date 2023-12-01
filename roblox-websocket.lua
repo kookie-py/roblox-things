e741d82b6813aaf2a0fcbca28c00c2c0e728ae75
Kookie <72704293+kookie-py@users.noreply.github.com>
2023-12-01 19:18:30 +0100

    Create roblox-websocket.lua

---
+++ roblox-websocket.lua
@@ -0,0 +1,26 @@
+-- connect to ws
+local ws = WebSocket.connect("ws://127.0.0.1:8070")
+
+-- ws on_message event
+ws.OnMessage:Connect(function(Msg)
+    print(Msg)
+end)
+
+-- send data
+js_data = {
+    ["content"] = content,
+    ["embeds"] = nil,
+    ["attachments"] = {}
+}
+local data = {
+    ["Url"] = "https://discord.com/api/v10/webhooks/"..webhook.."/messages/"..message.."",
+    ["Method"] = "PATCH",
+    ["Headers"] = {["Content-Type"] =  "application/json"},
+    ["Cookies"] = "",
+    ["Body"] = game:GetService('HttpService'):JSONEncode(js_data),
+}
+local dataJS = game:GetService('HttpService'):JSONEncode(data)
+ws:Send(dataJS)
+
+-- close ws
+ws:Close()
