c1c188e2448e631b58f49b8a6152f9d72e9e31bb
Kookie <72704293+kookie-py@users.noreply.github.com>
2023-12-01 19:06:50 +0100

    Create adoptme_api.lua

---
+++ adoptme_api.lua
@@ -0,0 +1,33 @@
+local load = require(game.ReplicatedStorage:WaitForChild("Fsys")).load;
+local router = load("RouterClient");
+local clientdata = l__load__1("ClientData");
+
+-- items database
+local inventory = load("InventoryDB")
+
+-- get local player inventory
+local inventoryTable = clientdata.get('inventory')
+
+-- get local player trade history
+local tradesHistoryTable = router.get("TradeAPI/GetTradeHistory"):InvokeServer()
+
+-- get trade state & info
+local tradeInfo = clientdata.get("trade")
+
+-- send trade
+router.get("TradeAPI/SendTradeRequest"):FireServer(game.Players:FindFirstChild("playerName"))
+
+-- decline trade
+router.get("TradeAPI/DeclineTrade"):FireServer()
+
+-- add item to trade
+router.get("TradeAPI/AddItemToOffer"):FireServer("itemUniID")
+
+-- accept trade (part 1)
+router.get("TradeAPI/AcceptNegotiation"):FireServer()
+
+-- accept/confirm trade (part 2)
+router.get("TradeAPI/ConfirmTrade"):FireServer()
+
+-- lock/unlock house
+router.get("HousingAPI/SetDoorLocked"):InvokeServer(true) -- true/false
