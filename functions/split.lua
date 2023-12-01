8e02abe58ee5eb0f23e7364772f66f4d09572515
Kookie <72704293+kookie-py@users.noreply.github.com>
2023-12-01 19:20:38 +0100

    Create split.lua

---
+++ functions/split.lua
@@ -0,0 +1,8 @@
+local function split(str, sep)
+   local result = {}
+   local regex = ("([^%s]+)"):format(sep)
+   for each in str:gmatch(regex) do
+      table.insert(result, each)
+   end
+   return result
+end
